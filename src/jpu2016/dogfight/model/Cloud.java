6a4b4fb769f651b41ab325cea79f709e852a847a
lulu <lulu@lucas>
2016-05-25 15:36:57 +0200

    remplissage de la moitié des classes

--- src/jpu2016/dogfight/model/Cloud.java
+++ src/jpu2016/dogfight/model/Cloud.java
@@ -2,4 +2,24 @@ package jpu2016.dogfight.model;
 
 public class Cloud {
 
+	private int SPEED = 1; 
+	private int WIDTH = 300; 
+	private int HEIGHT = 150;
+	private String IMAGE = "cloud";
+	public Cloud(Direction direction, Dimension dimension) {
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- src/jpu2016/dogfight/model/IArea.java
+++ src/jpu2016/dogfight/model/IArea.java
@@ -1,5 +1,10 @@
 package jpu2016.dogfight.model;
 
+import java.awt.Image;
+
 public interface IArea {
 
+
+	public Dimension getDimension();
+	public Image getImage();
 }
